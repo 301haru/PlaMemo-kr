8559c9a86935583b4eb0f99dd474e2efa8d78a69
Haru301 <39460338+seungjin1302@users.noreply.github.com>
2022-01-02 20:16:17 +0900

    Create LICENSE

--- src/com/haruww/plamemokr/Main.java
+++
@@ -1,9 +0,0 @@
-package com.haruww.plamemokr;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("TEST");
-    }
-}
---
+++ src/com/haruww/plamemokr/PlaMemoKR.java
@@ -0,0 +1,36 @@
+package com.haruww.plamemokr;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PlaMemoKR
+{
+    public static void main(String[] args) throws IOException, ParseException
+    {
+        Reader json = new FileReader("./scenario_json/루트 공통/pm01_01.txt.scn.json");
+
+
+        JSONParser jParser = new JSONParser();
+        JSONObject jObject = (JSONObject) jParser.parse(json);
+        JSONArray jArray = (JSONArray) jObject.get("scenes");
+
+        jObject = (JSONObject) jParser.parse(jArray.get(0).toString());
+        JSONArray jArray1 = (JSONArray) jObject.get("texts");
+
+
+        for(int i=0; i<jArray1.size(); i++)
+        {
+            jArray = (JSONArray) jArray1.get(i);//0부터 텍스트 쭉 나옴
+            if(jArray.get(0) == null && jArray.get(1) == null)
+            {
+                System.out.println(jArray.get(2));//0, 1은 null 2는 텍스트
+            }
+        }
+    }
+}

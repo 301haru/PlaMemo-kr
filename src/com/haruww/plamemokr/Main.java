88229102d13b90b1380b1b36f2d96943df7abb5a
Haru301 <39460338+seungjin1302@users.noreply.github.com>
2022-01-02 15:20:48 +0900

    Initial commit

--- src/com/haruww/plamemokr/Main.java
+++ src/com/haruww/plamemokr/Main.java
@@ -1,8 +1,9 @@
 package com.haruww.plamemokr;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("TEST");
     }
 }
